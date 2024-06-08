a33840f5adab118d7815dbecbb9a0e3a6be679da
JSerrano01 <jaime.serrano2001@gmail.com>
2024-06-07 23:30:21 -0500

    Se agrega proyecto por primera vez

---
+++ src/sections/CostumerReviews.jsx
@@ -0,0 +1,7 @@
+const CostumerReviews = () => {
+    return (
+        <div></div>
+    )
+}
+
+export default CostumerReviews
---
+++ src/sections/index.js
@@ -0,0 +1,21 @@
+import Hero from './Hero'
+import CostumerReviews from './CostumerReviews'
+import Footer from './Footer'
+import PopularProducts from './PopularProducts'
+import Services from './Services'
+import SpecialOffer from './SpecialOffer'
+import Subscribe from './Subscribe'
+import SuperQuality from './SuperQuality'
+import Nav from '../components/Nav'
+
+export {
+    Hero,
+    PopularProducts,
+    SuperQuality,
+    Subscribe,
+    CostumerReviews,
+    Footer,
+    Services,
+    SpecialOffer,
+    Nav
+}
